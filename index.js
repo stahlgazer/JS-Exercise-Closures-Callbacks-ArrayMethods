df1469574d3cac7934ce7e094b5e53a507ead586
gavin stahl <stahlgavin@gmail.com>
2019-11-12 15:19:02 -0800

    9 passes completed

--- index.js
+++ index.js
@@ -180,8 +180,12 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  let namerunners = [];
+  runners.forEach(function (item) {
+    namerunners.push(`${item.last_name}, ${item.first_name}`)
+  });
+  return namerunners;
 }
 
 /**
